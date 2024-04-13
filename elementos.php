2fd9ef8ad9f58a72c999ede97b0e3b51fdc62aa6
José Yzarra <jgyzarra@gmail.com>
2024-04-13 17:44:39 -0400

    pasando otros archivos

--- elementos.php
+++ elementos.php
@@ -2,8 +2,5 @@
 
 echo "otra información importante";
 
-<<<<<<< Updated upstream
 echo "<buttom clas'btn btn-primary'>Prueba de Boton</buttom>";
-=======
 echo "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio asperiores, tempora corporis quasi ipsam tempore beatae facilis ex culpa ab veritatis reprehenderit veniam, suscipit doloribus dolorum ipsum. Dicta, cum suscipit!";
->>>>>>> Stashed changes
